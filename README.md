4edcdc86def99604303b7f71376e6f3b07c4b050
Tyson Toller <90421293+HoneyTbone@users.noreply.github.com>
2022-10-23 12:12:36 -0400

    Custom Elements are not working with Debug... Testing for netlify
    Added ReadME Description
    
    Update to Header
    Update to Footer

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Portfolio
-A collection of all my work
+# Tyson Toller's Game Design Portfolio
+
+Website - <b>TysonToller.Netlify.App</b>
+
+NAV________________________________NAV
+
+../index    | Default page
+../Projects | Home Page
+../Resume   | contact info and Resume
+../AboutMe  | Fun Facts
+______________________________________
+
+Future Domain TysonToller.com/
+
+<u>A collection of all my Game Design work, which includes</u>
+Stuck at Work - Senior Capstone - VR - Escape Room
+Maze's End - Design Project - Board - 2-4 Player
+Pixel Leap - Hobby Project - Mobile - Vertical Platformer
+
+- Tyson Toller
+
+Email - <a href="mailto:Honeytbone33@gmail.com">Honeytbone33@gmail.com</a>
+LinkedIN - <a href="https://www.linkedin.com/in/tyson-toller-838377157/">Tyson Toller</a>
--- public_html/components.js
+++ public_html/components.js
@@ -2,10 +2,14 @@ class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <header>
-        <h1>Tyson Toller's Game Design Portfolio</h1>
-        <a href = "projects.html" > <button>PROJECTS</button></a >
-        <a href="aboutme.html"><button>ABOUT ME</button></a>
-        <a href="resume.html"><button>RESUME</button></a>
+            <div class="header">
+                <div class="head">
+                    <h1 margin:0>Tyson Toller's Game Design Portfolio</h1>
+                    <a href="projects.html"> <button>PROJECTS</button></a>
+                    <a href="aboutme.html"><button>ABOUT ME</button></a>
+                    <a href="resume.html"><button>RESUME</button></a>
+                </div>
+            </div>
         </header>
     `
     }
@@ -16,7 +20,7 @@ customElements.define('my-header', MyHeader)
 class MyFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <footer>
+        <footer class="footer">
         © 2022 - Tyson Toller - HoneyTbone33@gmail.com
         </footer>
     `
