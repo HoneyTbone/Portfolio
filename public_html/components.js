70d8aeddbe55d0c92e4281d26242e7a35584496f
Tyson Toller <90421293+HoneyTbone@users.noreply.github.com>
2022-11-07 21:12:14 -0500

    Finally Fixed all the annoying stuff, Testing to see if it broke stuff

--- public_html/components.js
+++ public_html/components.js
@@ -1,16 +1,16 @@
 class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
+        <center>
         <header>
-            <div class="header">
-                <div class="head">
-                    <h1 margin:0>Tyson Toller's Game Design Portfolio</h1>
-                    <a href="projects.html"> <button>PROJECTS</button></a>
-                    <a href="aboutme.html"><button>ABOUT ME</button></a>
-                    <a href="resume.html"><button>RESUME</button></a>
-                </div>
+            <div class="head">
+                <h1>Tyson Toller's Game Design Portfolio</h1>
+                <a href="projects.html"> <button>PROJECTS</button></a>
+                <a href="aboutme.html"><button>ABOUT ME</button></a>
+                <a href="resume.html"><button>RESUME</button></a>
             </div>
         </header>
+    </center>
     `
     }
 }
@@ -21,7 +21,7 @@ class MyFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <footer class="footer">
-        © 2022 - Tyson Toller - HoneyTbone33@gmail.com - Design inspired by www.michaellevall.com/
+        <div style="text-align:left">© 2022 - Tyson Toller - HoneyTbone33@gmail.com</div> <div style="text-align:right">Designed and Managed by Tyson Toller</div>
         </footer>
     `
     }
